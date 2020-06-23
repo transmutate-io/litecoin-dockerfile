3aab8c12954a78fb4e09192b7d8438c1c75a284f
transmutateio <transmutate.io@gmail.com>
2020-06-23 13:16:52 +0100

    use binaries instead of building from source version bump to 0.18.1, debian to 10.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # default is to call litecoind withoud args or with the provided args
-if [ "$1" = "" ] || [ "$(echo $1|cut -b 1)" = "-" ]; then
+if [ "$(echo $1|cut -b 1)" = "-" ]; then
   set -- litecoind "$@"
-else
-  exec "$@"
 fi
+
+exec "$@"
